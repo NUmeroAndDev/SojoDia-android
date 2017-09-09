5cbd3aa65a7e6119ca8b436cf9679604c73165d2
NUmeroAndDev <numero.dev.and@gmail.com>
2017-09-09 22:26:23 +0900

    リストのイニシャライズ処理を追加

--- app/src/main/java/com/numero/sojodia/manager/BusDataManager.java
+++ app/src/main/java/com/numero/sojodia/manager/BusDataManager.java
@@ -4,25 +4,24 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import android.support.annotation.NonNull;
 
+import com.numero.sojodia.model.BusDataFile;
 import com.numero.sojodia.model.BusTime;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Predicate;
 
 public class BusDataManager extends ContextWrapper {
 
-    private List<BusTime> tkBusTimeListOnWeekdayGoing = new ArrayList<>();
-    private List<BusTime> tkBusTimeListOnSaturdayGoing = new ArrayList<>();
-    private List<BusTime> tkBusTimeListOnSundayGoing = new ArrayList<>();
-    private List<BusTime> tkBusTimeListOnWeekdayReturn = new ArrayList<>();
-    private List<BusTime> tkBusTimeListOnSaturdayReturn = new ArrayList<>();
-    private List<BusTime> tkBusTimeListOnSundayReturn = new ArrayList<>();
-    private List<BusTime> tndBusTimeListOnWeekdayGoing = new ArrayList<>();
-    private List<BusTime> tndBusTimeListOnSaturdayGoing = new ArrayList<>();
-    private List<BusTime> tndBusTimeListOnSundayGoing = new ArrayList<>();
-    private List<BusTime> tndBusTimeListOnWeekdayReturn = new ArrayList<>();
-    private List<BusTime> tndBusTimeListOnSaturdayReturn = new ArrayList<>();
-    private List<BusTime> tndBusTimeListOnSundayReturn = new ArrayList<>();
+    private List<BusTime> tkBusTimeListGoing = new ArrayList<>();
+    private List<BusTime> tkBusTimeListReturn = new ArrayList<>();
+    private List<BusTime> tndBusTimeListGoing = new ArrayList<>();
+    private List<BusTime> tndBusTimeListReturn = new ArrayList<>();
+
+    private DataManager dataManager;
 
     private static BusDataManager INSTANCE;
 
@@ -35,5 +34,59 @@ public class BusDataManager extends ContextWrapper {
 
     public BusDataManager(Context context) {
         super(context);
+        dataManager = DataManager.getInstance(context);
+        initBusTimeList();
+    }
+
+    private void initBusTimeList() {
+        setBusTimeList(tkBusTimeListGoing, dataManager.getBusTimeData(BusDataFile.TK_TO_KUTC.getFileName()));
+        setBusTimeList(tkBusTimeListReturn, dataManager.getBusTimeData(BusDataFile.KUTC_TO_TK.getFileName()));
+        setBusTimeList(tndBusTimeListGoing, dataManager.getBusTimeData(BusDataFile.TND_TO_KUTC.getFileName()));
+        setBusTimeList(tndBusTimeListReturn, dataManager.getBusTimeData(BusDataFile.KUTC_TO_TND.getFileName()));
+    }
+
+    public List<BusTime> getTkGoingBusTimeList(int week) {
+        return getFilteredBusTimeList(tkBusTimeListGoing, week);
+    }
+
+    public List<BusTime> getTkReturnBusTimeList(int week) {
+        return getFilteredBusTimeList(tkBusTimeListReturn, week);
+    }
+
+    public List<BusTime> getTndGoingBusTimeList(int week) {
+        return getFilteredBusTimeList(tndBusTimeListGoing, week);
+    }
+
+    public List<BusTime> getTndReturnBusTimeList(int week) {
+        return getFilteredBusTimeList(tndBusTimeListReturn, week);
+    }
+
+    private List<BusTime> getFilteredBusTimeList(List<BusTime> busTimeList, final int week) {
+        return Observable.fromIterable(busTimeList)
+                .filter(new Predicate<BusTime>() {
+                    @Override
+                    public boolean test(@io.reactivex.annotations.NonNull BusTime busTime) throws Exception {
+                        return busTime.week == week;
+                    }
+                })
+                .toList()
+                .blockingGet();
+    }
+
+    // テキストからマッピング処理
+    private void setBusTimeList(List<BusTime> busTimeList, String busTimeSource) {
+        busTimeList.clear();
+        String lines[] = busTimeSource.split("\n");
+        for (String line : lines) {
+            StringTokenizer stringTokenizer = new StringTokenizer(line, ",");
+
+            int hour = Integer.valueOf(stringTokenizer.nextToken());
+            int minutes = Integer.valueOf(stringTokenizer.nextToken());
+            int week = Integer.valueOf(stringTokenizer.nextToken());
+            boolean isNonstop = Integer.valueOf(stringTokenizer.nextToken()) != 0;
+
+            BusTime busTime = new BusTime(hour, minutes, week, isNonstop);
+            busTimeList.add(busTime);
+        }
     }
 }
