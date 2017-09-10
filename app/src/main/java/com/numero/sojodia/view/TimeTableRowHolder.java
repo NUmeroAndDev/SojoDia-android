2cdb65e8862df76c99c8711a2e022a5d7db893c2
NUmeroAndDev <numero.dev.and@gmail.com>
2017-09-10 21:23:37 +0900

    ViewHolderの修正

--- app/src/main/java/com/numero/sojodia/view/TimeTableRowHolder.java
+++ app/src/main/java/com/numero/sojodia/view/TimeTableRowHolder.java
@@ -11,45 +11,35 @@ import com.numero.sojodia.model.TimeTableRow;
 
 public class TimeTableRowHolder extends RecyclerView.ViewHolder {
 
+    private View rowLayout;
+    private TextView hourTextView;
+    private TextView weekdayTextView;
+    private TextView saturdayTextView;
+    private TextView sundayTextView;
+
     public TimeTableRowHolder(View view) {
         super(view);
+
+        rowLayout = view.findViewById(R.id.row_layout);
+        hourTextView = view.findViewById(R.id.hour_text);
+        weekdayTextView = view.findViewById(R.id.weekday_text);
+        saturdayTextView = view.findViewById(R.id.saturday_text);
+        sundayTextView = view.findViewById(R.id.sunday_text);
     }
 
     public void setTimeTableRow(TimeTableRow row) {
-        setHourText(row.hourText);
-        setTimeTextOnWeekday(row.timeTextOnWeekday);
-        setTimeTextOnSaturday(row.timeTextOnSaturday);
-        setTimeTextOnSunday(row.timeTextOnSunday);
+        hourTextView.setText(row.hourText);
+        weekdayTextView.setText(row.timeTextOnWeekday);
+        saturdayTextView.setText(row.timeTextOnSaturday);
+        sundayTextView.setText(row.timeTextOnSunday);
     }
 
     public void setIsCurrentHour(boolean isCurrentHour) {
-        View view = itemView.findViewById(R.id.row_layout);
         Context context = itemView.getContext();
         if (isCurrentHour) {
-            view.setBackgroundColor(ContextCompat.getColor(context, R.color.time_table_dialog_row_highlight));
+            rowLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.time_table_dialog_row_highlight));
         } else {
-            view.setBackground(null);
+            rowLayout.setBackground(null);
         }
     }
-
-    private void setHourText(String text) {
-        TextView textView = (TextView) itemView.findViewById(R.id.hour_text);
-        textView.setText(text);
-    }
-
-    private void setTimeTextOnWeekday(String text) {
-        TextView textView = (TextView) itemView.findViewById(R.id.weekday_text);
-        textView.setText(text);
-    }
-
-    private void setTimeTextOnSaturday(String text) {
-        TextView textView = (TextView) itemView.findViewById(R.id.saturday_text);
-        textView.setText(text);
-    }
-
-    private void setTimeTextOnSunday(String text) {
-        TextView textView = (TextView) itemView.findViewById(R.id.sunday_text);
-        textView.setText(text);
-    }
-
 }
