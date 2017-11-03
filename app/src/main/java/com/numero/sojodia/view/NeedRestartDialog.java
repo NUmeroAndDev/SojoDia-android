99d748b601cb04072425518d04eda20638885735
NUmeroAndDev <numero.dev.and@gmail.com>
2017-11-03 15:07:07 +0900

    不要なキャスト

--- app/src/main/java/com/numero/sojodia/view/NeedRestartDialog.java
+++ app/src/main/java/com/numero/sojodia/view/NeedRestartDialog.java
@@ -1,7 +1,6 @@
 package com.numero.sojodia.view;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 
 import com.numero.sojodia.R;
