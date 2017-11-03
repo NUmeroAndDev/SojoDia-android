09ca4106c6d8c28fbd5c13f50c6054bf998f3a1b
NUmeroAndDev <numero.dev.and@gmail.com>
2017-11-03 15:10:18 +0900

    アノテーションの追加

--- app/src/main/java/com/numero/sojodia/view/adapter/BusTimePagerAdapter.java
+++ app/src/main/java/com/numero/sojodia/view/adapter/BusTimePagerAdapter.java
@@ -1,6 +1,7 @@
 package com.numero.sojodia.view.adapter;
 
 import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,8 +25,9 @@ public class BusTimePagerAdapter extends PagerAdapter {
     }
 
     @SuppressLint("DefaultLocale")
+    @NonNull
     @Override
-    public Object instantiateItem(ViewGroup container, int position) {
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
         View view = LayoutInflater.from(container.getContext()).inflate(R.layout.pager_bus_time, null);
 
         if (position >= busTimeList.size()) {
@@ -71,7 +73,7 @@ public class BusTimePagerAdapter extends PagerAdapter {
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         container.removeView((View) object);
     }
 
@@ -84,12 +86,12 @@ public class BusTimePagerAdapter extends PagerAdapter {
     }
 
     @Override
-    public boolean isViewFromObject(View view, Object object) {
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
         return view == object;
     }
 
     @Override
-    public int getItemPosition(Object object) {
+    public int getItemPosition(@NonNull Object object) {
         return POSITION_NONE;
     }
 }
