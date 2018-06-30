8550986740d7188d46afcae5bb2bc9095296714c
NUmeroAndDev <numero.dev.and@gmail.com>
2018-06-30 15:32:50 +0900

    不要になったものを削除

--- app/src/main/kotlin/com/numero/sojodia/fragment/RoundedBottomSheetFragment.kt
+++ app/src/main/kotlin/com/numero/sojodia/fragment/RoundedBottomSheetFragment.kt
@@ -2,7 +2,6 @@ package com.numero.sojodia.fragment
 
 import android.app.Dialog
 import android.os.Bundle
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.numero.sojodia.R
 
