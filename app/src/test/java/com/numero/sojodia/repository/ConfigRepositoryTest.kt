bd6e4b6d4acdc1b80b6e2571f52aac5d66e55289
NUmeroAndDev <numero.dev.and@gmail.com>
2018-11-29 10:14:12 +0900

    ConfigRepositoryのテスト追加

--- app/src/test/java/com/numero/sojodia/repository/ConfigRepositoryTest.kt
+++ app/src/test/java/com/numero/sojodia/repository/ConfigRepositoryTest.kt
@@ -2,7 +2,7 @@ package com.numero.sojodia.repository
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,7 +18,26 @@ class ConfigRepositoryTest {
     }
 
     @Test
-    fun `Hoge`() {
-        assertEquals(configRepository.versionCode, 20170401L)
+    fun `versionCode_デフォルトのバージョンコードが返って来ること`() {
+        assertEquals(configRepository.versionCode, ConfigRepository.DEFAULT_DATA_VERSION)
     }
+
+    @Test
+    fun `versionCode_保存されること`() {
+        val versionCode = 20180101L
+        configRepository.versionCode = versionCode
+        assertEquals(configRepository.versionCode, versionCode)
+    }
+
+    @Test
+    fun `isTodayUpdateChecked_初めての更新チェックでfalseが返ってくること`() {
+        assertFalse(configRepository.isTodayUpdateChecked)
+    }
+
+    @Test
+    fun `isTodayUpdateChecked_日付を更新するとtrueが返ってくること`() {
+        configRepository.updateCheckUpdateDate()
+        assertTrue(configRepository.isTodayUpdateChecked)
+    }
+
 }
